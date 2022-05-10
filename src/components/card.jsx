f5870f728f4cf236bd772d93a0459e57dfc0de88
AV-Felipe <aerogrado@hotmail.com>
2022-05-10 19:09:38 -0300

    Implement styled-components on card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,67 @@
 import React, {useEffect, useState} from "react";
-import './card.css';
+import styled from "styled-components";
+
+
+const MainCardStyle = styled.li`
+    box-sizing: border-box;
+    width: 450px;
+    height: 150px;
+    padding: 0;
+    margin: 2px;
+    list-style: none;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    border-radius: 8px;
+    overflow: hidden;
+    background-color: ${props => {
+        switch(props.className) {
+            case 'Alive': 
+                return 'green';
+            case 'Dead':
+                return 'red';
+            case 'unknown':
+                return 'grey';
+        }
+
+        }
+    };
+`;
+
+const DeadBanner = styled.p`
+    background-color: red;
+    box-shadow: rgba(0, 0, 0, 0.70) 0px 0px 5px 90px;
+    padding: 3px;
+`;
+
+const ImageHolder = styled.div`
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    width: 150px;
+    height: 150px;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    background-image: url(${props => props.img});
+`;
+
+const TextArea = styled.div`
+    list-style: none;
+    padding: 3px;
+`;
+
+const CharacterName = styled.h2`
+    font-size: 32px;
+    font-weight: bold;
+`;
+
+const CharacterSpecies = styled.p`
+    font-size: 16px;
+`
+
 
 function Card ({url}) {
 
@@ -25,24 +87,26 @@ function Card ({url}) {
     },[]);
 
     return (
-        <li className={"mainCard " + character.status}>
-            <div className="imageHolder" style={{ backgroundImage: `url(${character.image})` }}>
-            {character.status === "Dead"? <p className="deadBanner">Morto</p> : ""}
-            </div>
+        <MainCardStyle className={character.status}>
+        
+            <ImageHolder img={character.image}>
+            {character.status === "Dead"? <DeadBanner>Morto</DeadBanner> : ""}
+            </ImageHolder>
 
-            <div className="textArea">
+            <TextArea>
 
-                <h2 className="nameText">
+                <CharacterName>
                     {character.name}
-                </h2>
+                </CharacterName>
 
-                <p className="speciesText">
+                <CharacterSpecies>
                     {character.species}
-                </p>
+                </CharacterSpecies>
 
-            </div>
+            </TextArea>
 
-        </li>
+        
+        </MainCardStyle>
     )
 }
 
